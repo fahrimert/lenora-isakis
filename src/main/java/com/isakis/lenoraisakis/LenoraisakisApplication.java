bfde76308ddc3b87c9f4a329ad822081ba1864d2
Vimadd <fmtunckaya@gmail.com>
2025-10-07 10:30:17 +0300

    isakis proje spring boot initialization

---
+++ src/main/java/com/isakis/lenoraisakis/LenoraisakisApplication.java
@@ -0,0 +1,13 @@
+package com.isakis.lenoraisakis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenoraisakisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenoraisakisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isakis/lenoraisakis/LenoraisakisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isakis.lenoraisakis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenoraisakisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
